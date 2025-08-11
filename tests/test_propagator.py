7bbb319d148581c3e7488fb4ef95f84ac84c35d2
Jonas Streckis <jonas.str@outlook.com>
2025-08-11 21:28:37 +0100

    Add send event tests

--- tests/test_propagator.py
+++ tests/test_propagator.py
@@ -1,32 +1,34 @@
 import json
 import pytest
+import aiohttp
+from yarl import URL
+from aioresponses import aioresponses
 from unittest.mock import patch, mock_open
 
 from eventpropagator.models import Config
 from eventpropagator.propagator import EventPropagator
 
+@pytest.fixture(scope="module")
+def config():
+    return Config(
+        interval_seconds=1,
+        api_endpoint="http://test.com/event",
+        events_file="test_events.json"
+    )
 
-class TestEventPropagator:
-    @pytest.fixture
-    def config(self):
-        return Config(
-            interval_seconds=1,
-            api_endpoint="http://test.com/event",
-            events_file="test_events.json"
-        )
-    
-    @pytest.fixture
-    def propagator(self, config):
-        return EventPropagator(config)
-    
-    @pytest.fixture
-    def sample_events(self):
-        return [
-            {"event_type": "message", "event_payload": "hello"},
-            {"event_type":123, "event_payload":{}},
-            {"foo": "bar"},
-        ]
-    
+@pytest.fixture(scope="module")
+def propagator(config):
+    return EventPropagator(config)
+
+@pytest.fixture(scope="module")
+def sample_events():
+    return [
+        {"event_type": "message", "event_payload": "hello"},
+        {"event_type":123, "event_payload":{}},
+        {"foo": "bar"},
+    ]
+
+class TestLoadEvents:
     @pytest.mark.asyncio
     async def test_load_events_success(self, propagator, sample_events):
         with patch("pathlib.Path.exists", return_value=True):
@@ -47,3 +49,39 @@ class TestEventPropagator:
             with patch("builtins.open", mock_open(read_data="invalid json")):
                 with pytest.raises(json.JSONDecodeError):
                     await propagator.load_events()
+
+
+class TestSendEvents:
+    @pytest.mark.asyncio
+    async def test_send_event_success(self, propagator, sample_events):
+        event = sample_events[0]
+        
+        with aioresponses() as m:
+            m.post(propagator.config.api_endpoint, status=400)
+
+            propagator.session = aiohttp.ClientSession()
+
+            try:
+                await propagator.send_event(event)
+                
+                assert len(m.requests) == 1
+                request = m.requests[('POST', URL(propagator.config.api_endpoint))][0]
+                assert request.kwargs['json'] == event
+            finally:
+                await propagator.session.close()
+
+    @pytest.mark.asyncio
+    async def test_send_event_400(self, propagator, sample_events, caplog):
+        event = sample_events[0]
+        
+        with aioresponses() as m:
+            m.post(propagator.config.api_endpoint, status=400)
+            
+            propagator.session = aiohttp.ClientSession()
+            
+            try:
+                await propagator.send_event(event)
+                print(caplog)
+                assert "API responded with status 400" in caplog.text
+            finally:
+                await propagator.session.close()
